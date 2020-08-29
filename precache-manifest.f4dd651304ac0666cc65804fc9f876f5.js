6e2cea93bdb5c74f1e8d0d7ba6061f86dc0c3993
IkramMissaoui <missaoui.ikram726@gmail.com>
2020-08-29 03:13:08 +0200

    Updates

---
+++ precache-manifest.f4dd651304ac0666cc65804fc9f876f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044640b29758095994171e8b0ef15cb4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b0b3e57877f50cfc5ba6",
+    "url": "/my-app/static/css/main.604786ea.chunk.css"
+  },
+  {
+    "revision": "a903e1b8f032859213f8",
+    "url": "/my-app/static/js/2.ffd1124d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.ffd1124d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b3e57877f50cfc5ba6",
+    "url": "/my-app/static/js/main.c3e230db.chunk.js"
+  },
+  {
+    "revision": "962971831ce32967567c",
+    "url": "/my-app/static/js/runtime-main.646f5746.js"
+  }
+]);
